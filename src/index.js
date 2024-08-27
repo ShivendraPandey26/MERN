b1ea65496070122b296fb1929198ee40c7bdf6d7
Shivendra Kumar Pandey <aviralpandey7974@gmail.com>
2024-08-28 01:47:52 +0530

    setup and add middlewares

---
+++ src/index.js
@@ -0,0 +1,39 @@
+import { config } from "dotenv";
+config();
+import connectDB from "./db/index.js";
+import express from "express";
+import cookieParser from "cookie-parser";
+import cors from "cors";
+
+const app = express();
+
+// Environment variables
+const PORT = process.env.PORT || 8080;
+
+// Middleware
+app.use(cors());
+app.use(express.json());
+app.use(express.urlencoded({ extended: true }));
+app.use(express.static("Public"));
+app.use(cookieParser());
+
+// Routes
+app.get("/", (req, res) => {
+  res.send("Hello from the backend!");
+});
+
+// Connect to MongoDB
+connectDB()
+  .then(() => {
+    app.on("error", (err) => {
+      console.error("Server Error", err);
+      process.exit(1);
+    });
+    app.listen(PORT, () => {
+      console.log(`Server running on port ${PORT}`);
+    });
+  })
+  .catch((error) => {
+    console.error("Failed to connect to MongoDB!!!", error);
+    process.exit(1);
+  });
