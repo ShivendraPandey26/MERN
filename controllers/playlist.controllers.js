329f5a5978c83be995003e6c2545df06a77ca8d3
Shivendra Kumar Pandey <aviralpandey7974@gmail.com>
2024-09-20 00:06:51 +0530

    add new

--- controllers/playlist.controllers.js
+++ controllers/playlist.controllers.js
@@ -1,53 +1,89 @@
-// import mongoose, {isValidObjectId} from "mongoose"
-// import {Playlist} from "../models/playlist.model.js"
-// import {ApiError} from "../utils/ApiError.js"
-// import {ApiResponse} from "../utils/ApiResponse.js"
-import {asyncHandler} from "../utils/asyncHandler.js"
-
+import mongoose, { isValidObjectId } from "mongoose";
+import Playlist from "../models/playlist.model.js";
+import { ApiResponse } from "../utils/ApiResponse.js";
+import { asyncHandler } from "../utils/asyncHandler.js";
 
 const createPlaylist = asyncHandler(async (req, res) => {
-    const {name, description} = req.body
+  const { name, description } = req.body;
 
-    //TODO: create playlist
-})
+  if (!name || !description) {
+    return res
+      .status(400)
+      .json({ message: "Name and description are required" });
+  }
+
+  try {
+    const playlist = await Playlist.create({
+      name,
+      description,
+      owner: req.user._id, // Assuming req.user contains the authenticated user's info
+    });
+
+    return res.status(201).json({
+      status: 201,
+      data: playlist,
+      message: "Playlist created successfully",
+    });
+  } catch (error) {
+    return res.status(500).json({ message: "Failed to create playlist" });
+  }
+});
 
 const getUserPlaylists = asyncHandler(async (req, res) => {
-    const {userId} = req.params
-    //TODO: get user playlists
-})
+  const { userId } = req.params;
+  //TODO: get user playlists
+});
 
 const getPlaylistById = asyncHandler(async (req, res) => {
-    const {playlistId} = req.params
-    //TODO: get playlist by id
-})
+  const { playlistId } = req.params;
+
+  if (!isValidObjectId(playlistId)) {
+    return res.status(400).json({ message: "Invalid playlist ID" });
+  }
+
+  try {
+    const playlist = await Playlist.findById(playlistId);
+
+    if (!playlist) {
+      return res.status(404).json({ message: "Playlist not found" });
+    }
+
+    return res
+      .status(200)
+      .json(new ApiResponse(200, playlist, "playlist get successfully"));
+  } catch (error) {
+    return res
+      .status(500)
+      .json({ message: "Internal server error Failed to retrieve playlist" });
+  }
+});
 
 const addVideoToPlaylist = asyncHandler(async (req, res) => {
-    const {playlistId, videoId} = req.params
-})
+  const { playlistId, videoId } = req.params;
+});
 
 const removeVideoFromPlaylist = asyncHandler(async (req, res) => {
-    const {playlistId, videoId} = req.params
-    // TODO: remove video from playlist
-
-})
+  const { playlistId, videoId } = req.params;
+  // TODO: remove video from playlist
+});
 
 const deletePlaylist = asyncHandler(async (req, res) => {
-    const {playlistId} = req.params
-    // TODO: delete playlist
-})
+  const { playlistId } = req.params;
+  // TODO: delete playlist
+});
 
 const updatePlaylist = asyncHandler(async (req, res) => {
-    const {playlistId} = req.params
-    const {name, description} = req.body
-    //TODO: update playlist
-})
+  const { playlistId } = req.params;
+  const { name, description } = req.body;
+  //TODO: update playlist
+});
 
 export {
-    createPlaylist,
-    getUserPlaylists,
-    getPlaylistById,
-    addVideoToPlaylist,
-    removeVideoFromPlaylist,
-    deletePlaylist,
-    updatePlaylist
-}
+  createPlaylist,
+  getUserPlaylists,
+  getPlaylistById,
+  addVideoToPlaylist,
+  removeVideoFromPlaylist,
+  deletePlaylist,
+  updatePlaylist,
+};
