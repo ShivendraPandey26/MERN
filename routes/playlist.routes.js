02279dd0e548c76804f0325af9baab6f507bde0b
Shivendra Kumar Pandey <aviralpandey7974@gmail.com>
2024-09-20 00:06:55 +0530

    add new

--- routes/playlist.routes.js
+++ routes/playlist.routes.js
@@ -1,30 +1,30 @@
-import { Router } from 'express';
+import { Router } from "express";
 import {
-    addVideoToPlaylist,
-    createPlaylist,
-    deletePlaylist,
-    getPlaylistById,
-    getUserPlaylists,
-    removeVideoFromPlaylist,
-    updatePlaylist,
-} from "../controllers/playlist.controllers.js"
-import {verifyJWT} from "../middlewares/auth.middleware.js"
+  createPlaylist,
+  getPlaylistById,
+  getUserPlaylists,
+  updatePlaylist,
+  removeVideoFromPlaylist,
+  deletePlaylist,
+  addVideoToPlaylist,
+} from "../controllers/playlist.controllers.js";
+import { verifyJWT } from "../middleware/auth.middleware.js";
 
 const router = Router();
 
 router.use(verifyJWT);
 
-router.route("/").post(createPlaylist)
+router.route("/").post(createPlaylist);
 
 router
-    .route("/:playlistId")
-    .get(getPlaylistById)
-    .patch(updatePlaylist)
-    .delete(deletePlaylist);
+  .route("/:playlistId")
+  .get(getPlaylistById)
+  .patch(updatePlaylist)
+  .delete(deletePlaylist);
 
 router.route("/add/:videoId/:playlistId").patch(addVideoToPlaylist);
 router.route("/remove/:videoId/:playlistId").patch(removeVideoFromPlaylist);
 
 router.route("/user/:userId").get(getUserPlaylists);
 
-export default router
+export default router;
