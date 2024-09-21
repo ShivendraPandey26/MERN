405f561b9a7f48d4a3433de37a00c1244c7ede3c
Shivendra Kumar Pandey <aviralpandey7974@gmail.com>
2024-09-21 23:31:56 +0530

    add comments section

--- index.js
+++ index.js
@@ -30,6 +30,7 @@ import userRouter from "./routes/user.routes.js";
 import videoRouter from "./routes/video.routes.js"
 import tweetRouter from "./routes/tweet.routes.js";
 import playlistRouter from "./routes/playlist.routes.js"
+import commentRouter from "./routes/comment.routes.js"
 
 
 // routes declarations
@@ -37,6 +38,7 @@ app.use("/api/v1/users", userRouter);
 app.use("/api/v1/video", videoRouter);
 app.use("/api/v1/tweets", tweetRouter);
 app.use("/api/v1/playlist", playlistRouter);
+app.use("/api/v1/comments", commentRouter);
 
 
 
