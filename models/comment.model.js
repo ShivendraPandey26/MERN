857c8cb484d53f2443c2459fd7f5f6d160db14ac
Shivendra Kumar Pandey <aviralpandey7974@gmail.com>
2024-09-21 23:32:05 +0530

    add comments section

--- models/comment.model.js
+++ models/comment.model.js
@@ -3,17 +3,19 @@ import mongooseAggregatePaginate from "mongoose-aggregate-paginate-v2";
 
 const commentSchema = new Schema(
   {
-    content: {
+    comment: {
       type: String,
       required: true,
     },
     video: {
       type: Schema.Types.ObjectId,
       ref: "Video",
+      required: true,
     },
     owner: {
       type: Schema.Types.ObjectId,
       ref: "User",
+      required: true,
     },
   },
   { timestamps: true }
