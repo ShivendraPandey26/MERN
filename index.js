d3c7899103d887c4f1f68047196d8b020dac6db8
Shivendra Kumar Pandey <aviralpandey7974@gmail.com>
2024-09-29 22:45:04 +0530

    add cors url

--- index.js
+++ index.js
@@ -15,7 +15,7 @@ const PORT = process.env.PORT || 8080;
 
 // Middleware
 app.use(cors({
-  origin: ['http://localhost:5173', '*'],
+  origin: ['http://localhost:5173', 'https://yt-c-lac.vercel.app', '*'],
   credentials: true
 }));
 app.use(express.json());
